05dd0b9782d81b11620ec8b4a828507b3dc70d83
zhengshiqiang <344871874@qq.com>
2024-05-15 20:34:30 +0800

    实现获取是否暴击或者格挡

---
+++ Source/Aura/Private/RPGAbilityTypes.cpp
@@ -0,0 +1,108 @@
+﻿#include "RPGAbilityTypes.h"
+
+bool FRPGGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
+{
+	uint32 RepBits = 0;
+	
+	if (Ar.IsSaving())
+	{
+		if (bReplicateInstigator && Instigator.IsValid())
+		{
+			RepBits |= 1 << 0;
+		}
+		if (bReplicateEffectCauser && EffectCauser.IsValid() )
+		{
+			RepBits |= 1 << 1;
+		}
+		if (AbilityCDO.IsValid())
+		{
+			RepBits |= 1 << 2;
+		}
+		if (bReplicateSourceObject && SourceObject.IsValid())
+		{
+			RepBits |= 1 << 3;
+		}
+		if (Actors.Num() > 0)
+		{
+			RepBits |= 1 << 4;
+		}
+		if (HitResult.IsValid())
+		{
+			RepBits |= 1 << 5;
+		}
+		if (bHasWorldOrigin)
+		{
+			RepBits |= 1 << 6;
+		}
+		//自定义内容，增加暴击和格挡触发存储
+		if(bIsBlockedHit)
+		{
+			RepBits |= 1 << 7;
+		}
+		if(bIsCriticalHit)
+		{
+			RepBits |= 1 << 8;
+		}
+	}
+
+	//使用了多少长度，就将长度设置为多少
+	Ar.SerializeBits(&RepBits, 9);
+
+	if (RepBits & (1 << 0))
+	{
+		Ar << Instigator;
+	}
+	if (RepBits & (1 << 1))
+	{
+		Ar << EffectCauser;
+	}
+	if (RepBits & (1 << 2))
+	{
+		Ar << AbilityCDO;
+	}
+	if (RepBits & (1 << 3))
+	{
+		Ar << SourceObject;
+	}
+	if (RepBits & (1 << 4))
+	{
+		SafeNetSerializeTArray_Default<31>(Ar, Actors);
+	}
+	if (RepBits & (1 << 5))
+	{
+		if (Ar.IsLoading())
+		{
+			if (!HitResult.IsValid())
+			{
+				HitResult = TSharedPtr<FHitResult>(new FHitResult());
+			}
+		}
+		HitResult->NetSerialize(Ar, Map, bOutSuccess);
+	}
+	if (RepBits & (1 << 6))
+	{
+		Ar << WorldOrigin;
+		bHasWorldOrigin = true;
+	}
+	else
+	{
+		bHasWorldOrigin = false;
+	}
+	//新增对暴击格挡的序列化或反序列化处理
+	if (RepBits & (1 << 7))
+	{
+		Ar << bIsBlockedHit;
+	}
+	if (RepBits & (1 << 8))
+	{
+		Ar << bIsCriticalHit;
+	}
+
+	if (Ar.IsLoading())
+	{
+		AddInstigator(Instigator.Get(), EffectCauser.Get()); // Just to initialize InstigatorAbilitySystemComponent
+	}	
+
+	bOutSuccess = true;
+	return true;
+}
---
+++ Source/Aura/Public/RPGAbilityTypes.h
@@ -0,0 +1,57 @@
+﻿#pragma once //预处理指令 确保这个头文件只被包含（include）一次，防止重复定义。
+
+#include "GameplayEffectTypes.h"
+#include "RPGAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType) //在蓝图中可作为类型使用
+struct FRPGGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY() //宏 自动生成构造函数、析构函数、拷贝构造函数等
+
+public:
+
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+
+	void SetIsBlockedHit(const bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+	void SetIsCriticalHit(const bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+	
+	/** 返回用于序列化的实际结构体 */
+	virtual UScriptStruct* GetScriptStruct() const override
+	{
+		return FGameplayEffectContext::StaticStruct();
+	}
+
+	/** 用于序列化类的参数 */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
+
+	/** 创建一个副本，用于后续网络复制或者后续修改 */
+	virtual FRPGGameplayEffectContext* Duplicate() const override
+	{
+		FRPGGameplayEffectContext* NewContext = new FRPGGameplayEffectContext();
+		*NewContext = *this;//WithCopy 设置为true，就可以通过赋值操作进行拷贝
+		if (GetHitResult())
+		{
+			// 深拷贝 hit result
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
+	}
+protected:
+
+	UPROPERTY()
+	bool bIsBlockedHit = false; //格挡
+
+	UPROPERTY()
+	bool bIsCriticalHit = false; //暴击
+};
+
+template<>
+struct TStructOpsTypeTraits< FRPGGameplayEffectContext > : public TStructOpsTypeTraitsBase2< FRPGGameplayEffectContext >
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true		// Necessary so that TSharedPtr<FHitResult> Data is copied around
+	};
+};
